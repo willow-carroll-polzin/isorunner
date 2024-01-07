1cb74ee93e89cd44fe3bec90a26995064361e684
Willow <willowcarrollpolzin@gmail.com>
2024-01-07 15:53:43 -0500

    Trying to get frog to workkkk

--- Enemies/Frog.gd
+++ Enemies/Frog.gd
@@ -13,34 +13,50 @@ var chase_flag = false # Not chasing the player if false
 func _on_player_detection_area_body_entered(body):
 	if body.name == "Player": # If player is detected
 		chase_flag = true
+		player = get_node("../../Player/Player")
 
 # Detect the player exiting area
 func _on_player_detection_area_body_exited(body):
 	if body.name == "Player": # If player is detected
 		chase_flag = false
+		player = get_node("../../Player/Player")
 
 func _physics_process(delta):
 	# Add GRAVITY
-	velocity.y += GRAVITY * delta
+	self.velocity.y += GRAVITY * delta
+	var v2 = GRAVITY - 2
 
+	print("DIRECTION: ")
 	print(direction)
+	print("SELF: ", str(self.position))
 	
 	# Check if we need to chase the player
 	if chase_flag == true:
 		# Determine direction to player
 		player = get_node("../../Player/Player")
+		
 		print("self pre: ", str(self.position))
 		print("pre: ", str(player.position))
-		direction = (self.position-player.position).normalized() 
+		
+		print("self pre N: ", str(self.position.normalized()))
+		print("pre N: ", str(player.position.normalized()))
+		direction = self.position-player.position.normalized() 
+		
 		print("self post: ", str(self.position))
 		print("pre: ", str(player.position))
+		print("DIRECTIONNNN: ")
+		print(direction)
+		var d2 = self.position-player.position
+		print("DDDDDDD: ")
+		print(d2)
 		
 		if direction.x > 0: # On the right of the player
 			get_node("AnimatedSprite2D").flip_h = true
-			velocity.x = direction.x * SPEED
+			#self.velocity.x = direction.x * SPEED
 		elif direction.x < 0: # On the left of the player
 			get_node("AnimatedSprite2D").flip_h = false
-			velocity.x = direction.x * SPEED
+			#self.velocity.x = direction.x * SPEED
 	else:
-		velocity.x = 0
+		self.velocity.x = 0
+		get_node("AnimatedSprite2D").play("Idle")
 	move_and_slide()
