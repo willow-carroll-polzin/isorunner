b4d57e74e12049e9d3ad5d75ab50b9280a2795bc
Willow <willowcarrollpolzin@gmail.com>
2024-01-07 17:20:58 -0500

    FINALLY got the frog working, one major issue was in the world.tscn they were each a child node to a larger orginzation node for player and mob and that seemed to cause a coordinate mismatch that was not resolving in either global or local methods

--- Enemies/frog.gd
+++ Enemies/frog.gd
@@ -13,42 +13,26 @@ var chase_flag = false # Not chasing the player if false
 func _on_player_detection_area_body_entered(body):
 	if body.name == "Player": # If player is detected
 		chase_flag = true
-		player = get_node("../../Player/Player")
 
 # Detect the player exiting area
 func _on_player_detection_area_body_exited(body):
 	if body.name == "Player": # If player is detected
 		chase_flag = false
-		player = get_node("../../Player/Player")
 
 func _physics_process(delta):
 	# Add GRAVITY
 	self.velocity.y += GRAVITY * delta
-	var v2 = GRAVITY - 2
-
-	print("DIRECTION: ")
-	print(direction)
-	print("SELF: ", str(self.position))
 	
 	# Check if we need to chase the player
 	if chase_flag == true:
 		# Determine direction to player
-		player = get_node("../../Player/Player")
+		player = get_parent().get_node("Player")
 		
-		print("self pre: ", str(self.position))
-		print("pre: ", str(player.position))
-		
-		print("self pre N: ", str(self.position.normalized()))
-		print("pre N: ", str(player.position.normalized()))
-		direction = self.position-player.position.normalized() 
-		
-		print("self post: ", str(self.position))
-		print("pre: ", str(player.position))
-		print("DIRECTIONNNN: ")
-		print(direction)
-		var d2 = self.position-player.position
-		print("DDDDDDD: ")
-		print(d2)
+		# Convert positions to global coordinates
+		var self_global_position  = self.position
+		var player_global_position = player.position
+
+		direction = (player_global_position - self_global_position).normalized()
 		
 		if direction.x > 0: # On the right of the player
 			get_node("AnimatedSprite2D").flip_h = true
@@ -56,7 +40,9 @@ func _physics_process(delta):
 		elif direction.x < 0: # On the left of the player
 			get_node("AnimatedSprite2D").flip_h = false
 			#self.velocity.x = direction.x * SPEED
+		get_node("AnimatedSprite2D").play("Jump")
+		self.velocity.x = direction.x * SPEED
 	else:
-		self.velocity.x = 0
 		get_node("AnimatedSprite2D").play("Idle")
+		self.velocity.x = 0 
 	move_and_slide()
