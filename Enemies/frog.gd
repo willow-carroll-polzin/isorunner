890ce2264b7f79fc6aef5ad086d0216e321b8f28
Willow <willowcarrollpolzin@gmail.com>
2024-01-29 15:22:34 -0500

    Working on frog death animation

--- Enemies/frog.gd
+++ Enemies/frog.gd
@@ -9,9 +9,13 @@ var chase_flag = false # Not chasing the player if false
 
 @onready var frog_animation = get_node("AnimatedSprite2D")
 
+# Setup the frog
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
 # Detect the player entering area
 func _on_player_detection_area_body_entered(body):
-	if body.name == "Player": # If player is detected
+	if body.name == "Player": # where does godot install linuxIf player is detected
 		chase_flag = true
 
 # Detect the player exiting area
@@ -19,9 +23,19 @@ func _on_player_detection_area_body_exited(body):
 	if body.name == "Player": # If player is detected
 		chase_flag = false
 
+#Check ea
+func _on_player_detection_area_for player collision/kill
+func _on_player_death_area_body_entered(body):
+	if body.name == "Player": #Player is colliding with hitbox
+		get_node("AnimatedSprite2D").play("Death")
+		await get_node("AnimatedSprite2D").animation_finished
+		self.queue_free() #Delete the frog
+
 func _physics_process(delta):
 	# Add GRAVITY
 	self.velocity.y += GRAVITY * delta
+	self.velocity.x = 0
+	chase_flag = false
 	
 	# Check if we need to chase the player
 	if chase_flag == true:
@@ -34,15 +48,18 @@ func _physics_process(delta):
 
 		direction = (player_global_position - self_global_position).normalized()
 		
-		if direction.x > 0: # On the right of the player
-			get_node("AnimatedSprite2D").flip_h = true
-			#self.velocity.x = direction.x * SPEED
-		elif direction.x < 0: # On the left of the player
-			get_node("AnimatedSprite2D").flip_h = false
-			#self.velocity.x = direction.x * SPEED
-		get_node("AnimatedSprite2D").play("Jump")
-		self.velocity.x = direction.x * SPEED
+		if get_node("AnimatedSprite2D").animation != "Death":
+			if direction.x > 0: # On the right of the player (sprite faces left by default)
+				get_node("AnimatedSprite2D").flip_h = true
+				#self.velocity.x = direction.x * SPEED
+			elif direction.x < 0: # On the left of the player
+				get_node("AnimatedSprite2D").flip_h = false
+				#self.velocity.x = direction.x * SPEED
+			get_node("AnimatedSprite2D").play("Jump")
+			self.velocity.x = direction.x * SPEED
 	else:
-		get_node("AnimatedSprite2D").play("Idle")
+		if get_node("AnimatedSprite2D").animation != "Death":
+			get_node("AnimatedSprite2D").play("Idle")
 		self.velocity.x = 0 
+		
 	move_and_slide()
